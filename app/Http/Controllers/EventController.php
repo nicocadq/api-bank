27c087b49afa5c2f5fb94ebf11b5f81a060dcbb7
nicocadq <nmachado.camejo@gmail.com>
2021-08-01 22:40:20 -0300

    Add trasnfer and withdrawal

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,9 +22,30 @@ class EventController extends Controller
                 $wallet->money = $wallet->money + $request->input('monto');
                 $wallet->save();
 
-                return response()->json(['id' => $wallet->id, 'balance' => $wallet->money], 201);
+                return response()->json(['id' => $wallet->id, 'balance' => $wallet->money], 200);
+            case $this->WITHDRAWAL:
+                $wallet = Wallet::findOrFail($request->input('origen'));
+
+                $wallet->money = $wallet->money - $request->input('monto');
+                $wallet->save();
+
+                return response()->json(['id' => $wallet->id, 'balance' => $wallet->money], 200);
+            case $this->TRANSFER:
+                $origin_wallet = Wallet::findOrFail($request->input('origen'));
+                $destiny_wallet = Wallet::findOrFail($request->input('destino'));
+
+                $origin_wallet->money = $origin_wallet->money - $request->input('monto');
+                $origin_wallet->save();
+
+                $destiny_wallet->money = $destiny_wallet->money + $request->input('monto');
+                $destiny_wallet->save();
+
+                return response()->json([
+                    ['id' => $origin_wallet->id, 'balance' => $origin_wallet->money], 
+                    ['id' => $destiny_wallet->id, 'balance' => $destiny_wallet->money]
+                ], 200);            
             default:
-             return response()->json('Not found', 404);
+                return response()->json('Not found', 404);
           }
     }
 }
--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -18,10 +18,43 @@ class EventTest extends TestCase
         $response = $this->post('/api/evento', ['tipo' => 'deposito', 'destino' => $wallet->id, 'monto' => $amount]);
 
         $response
-        ->assertStatus(201)
+        ->assertStatus(200)
         ->assertJson([
             'id' => $wallet->id,
             'balance' => ($wallet->money + $amount)
+        ]);
+    }
+
+    public function testCreateWithdrawal()
+    {
+        $amount = 100;
+        $wallet = Wallet::factory()->create();
+
+        $response = $this->post('/api/evento', ['tipo' => 'retiro', 'origen' => $wallet->id, 'monto' => $amount]);
+
+        $response
+        ->assertStatus(200)
+        ->assertJson([
+            'id' => $wallet->id,
+            'balance' => ($wallet->money - $amount)
+        ]);
+    }
+
+    public function testCreateTransfer()
+    {
+        $amount = 100;
+        $origin_wallet = Wallet::factory()->create();
+        $destiny_wallet = Wallet::factory()->create();
+
+        $response = $this->post('/api/evento', 
+            ['tipo' => 'transferencia', 'origen' => $origin_wallet->id, 'destino' => $destiny_wallet->id, 'monto' => $amount]
+        );
+
+        $response
+        ->assertStatus(200)
+        ->assertJson([
+            [ 'id' => $origin_wallet->id, 'balance' => ($origin_wallet->money - $amount)],
+            [ 'id' => $destiny_wallet->id, 'balance' => ($destiny_wallet->money + $amount)],
         ]);
     }
 }
